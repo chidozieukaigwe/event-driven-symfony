14ebed190ef1258fbf700f2d63d3b347c312a522
Chidozie Ukaigwe <chido@Chidozies-Air.lan>
2025-07-08 14:47:39 +0100

    Add Webhook folder to handle webhook dto - handlers and delegators

--- src/Controller/WebhooksController.php
+++ src/Controller/WebhooksController.php
@@ -4,23 +4,37 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\DTO\Webhook;
+use App\Webhook\Handler\HandlerDelegator;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * WebhooksController
  */
 class WebhooksController extends AbstractController
 {
+
+    public function __construct(private SerializerInterface $serializer, private HandlerDelegator $handlerDelegator) {}
     /**
      * Method healthcheck
      *
      * @return Response
      */
     #[Route(path: '/webhook', name: 'webhook', methods: ['POST'])]
-    public function __invoke(): Response
+    public function __invoke(Request $request): Response
     {
-        return new Response(status: Response::HTTP_NO_CONTENT);
+        try {
+            $webhook = $this->serializer->deserialize($request->getContent(), Webhook::class, 'json');
+            $webhook->setRawPayload($request->getContent());
+            $this->handlerDelegator->delegate($webhook);
+            return new Response(status: Response::HTTP_NO_CONTENT);
+        } catch (Exception $exception) {
+            throw $exception;
+        }
     }
 }
---
+++ src/Webhook/Handler/HandlerDelegator.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Webhook\Handler;
+
+use App\DTO\Webhook;
+use Symfony\Component\DependencyInjection\Attribute\AutowireIterator;
+
+class HandlerDelegator
+{
+    // Autowires an iterator of services based on a tag name
+
+    public function __construct(
+        #[AutowireIterator('webhook.handler')] private iterable $handlers
+    ) {}
+
+    public function delegate(Webhook $webhook): void
+    {
+        // Loop over handlers
+        foreach ($this->handlers as $handler) {
+            // Ask if supported (i.e call supports())
+            if ($handler->supports($webhook)) {
+                // if supported, call handle()
+                $handler->handle($webhook);
+            }
+        }
+    }
+}
