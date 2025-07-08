68fb1782aedb1a8af929851f786527b42bbed46f
Chidozie Ukaigwe <chido@Chidozies-Air.lan>
2025-07-08 12:34:34 +0100

    Add Http CdpClient

---
+++ src/CDP/Http/CdpClient.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\CDP\Http;
+
+use App\CDP\Analytics\Model\ModelInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+class CdpClient
+{
+    private const string CDP_API_URL = 'https://api.cdp.com/v1';
+
+    public function __construct(
+        private HttpClientInterface $httpClient,
+        #[Autowire(param: 'cdp.api_key')] private string $apiKey
+    ) {}
+
+    /**
+     * Method track
+     *
+     * @param ModelInterface $model [explicite description]
+     *
+     * @return void
+     */
+    public function track(ModelInterface $model): void
+    {
+        $this->httpClient->request(
+            'POST',
+            self::CDP_API_URL . '/track',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->apiKey,
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => json_encode($model->toArray(), JSON_THROW_ON_ERROR),
+            ]
+        );
+
+        // Add error handling
+    }
+
+    public function identify(ModelInterface $model): void
+    {
+        $this->httpClient->request(
+            'POST',
+            self::CDP_API_URL . '/identify',
+            [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $this->apiKey,
+                    'Content-Type' => 'application/json',
+                ],
+                'body' => json_encode($model->toArray(), JSON_THROW_ON_ERROR),
+            ]
+        );
+
+        // Add error handling
+
+    }
+}
