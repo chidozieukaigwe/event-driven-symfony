7b700f0aa4832fbc7464828c37515b234ddaebf4
Chidozie Ukaigwe <chido@Chidozies-Air.lan>
2025-07-08 11:04:48 +0100

    healthcheckk endpoint controller created

---
+++ src/Controller/StatusController.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+
+/**
+ * StatusController
+ */
+class StatusController extends AbstractController
+{
+
+    /**
+     * Method healthcheck
+     *
+     * @return JsonResponse
+     */
+    #[Route('/healthcheck', name: 'healthcheck', methods: ['GET'])]
+    public function healthcheck(): JsonResponse
+    {
+        return new JsonResponse(
+            [
+                'app' => true
+            ]
+        );
+    }
+}
