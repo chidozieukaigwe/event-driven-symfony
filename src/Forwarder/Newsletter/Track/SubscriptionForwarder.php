28471bcfcec21a6b1c79bd0651b3f08cb42638c0
Chidozie Ukaigwe <chido@Chidozies-Air.lan>
2025-07-09 10:01:12 +0100

    Add SubscriptionForwarder

---
+++ src/Forwarder/Newsletter/Track/SubscriptionForwarder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Forwarder\Newsletter\Track;
+
+use App\DTO\Newsletter\NewsletterWebhook;
+use App\Forwarder\Newsletter\ForwarderInterface;
+
+class SubscriptionForwarder implements ForwarderInterface
+{
+
+    public function supports(NewsletterWebhook $newsletterWebhook): bool
+    {
+        return true;
+    }
+
+    public function forward(NewsletterWebhook $newsletterWebhook): void
+    {
+        // Instantiate a class which models tracking data
+
+        // Map the NewsletterWebhook data to the model
+
+        // Validate the model
+
+        // Use the CDP client to POST the data to the CDP
+    }
+}
